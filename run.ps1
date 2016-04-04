92717555619df003bc25578c9279e238ad1af2b3
dhbrett <DHBDev@outlook.com>
2016-04-04 11:19:32 -0700

    Made run.ps1 quieter

--- run.ps1
+++ run.ps1
@@ -2,9 +2,9 @@ if(-Not (Test-Path .\nuget.exe)){
     (New-Object net.WebClient).DownloadFile('https://nuget.org/nuget.exe', 'nuget.exe')
 }
 
-nuget.exe restore A11y.sln
+nuget.exe restore A11y.sln -Verbosity quiet
 
 $msbuild = Get-Item C:\Windows\Microsoft.NET\Framework\v4*\MSBuild\
-& $msbuild .\A11y.sln
+& $msbuild .\A11y.sln /v:q /nologo
 
 .\bin\Debug\Microsoft.Edge.A11y.exe
