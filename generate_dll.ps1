569c5adb9d330d563a90734299177c03c9fc06fa
dhbrett <DHBDev@outlook.com>
2016-04-13 14:39:48 -0700

    Fixing typo in generate_dll

--- generate_dll.ps1
+++ generate_dll.ps1
@@ -3,6 +3,6 @@
     C:\Windows\System32\UIAutomationCore.dll /out:Interop.UIAutomationCore.dll
 }
 else { #32 bit
-    & 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\TlbImp.exe' `
+    & 'C:\Program Files\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\TlbImp.exe' `
     C:\Windows\System32\UIAutomationCore.dll /out:Interop.UIAutomationCore.dll
 }
