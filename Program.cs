0f29aea4a712d6182aa6be3602dbe724de055c89
dhbrett <DHBDev@outlook.com>
2016-04-13 15:04:00 -0700

    Fixing score calculation

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ namespace Microsoft.Edge.A11y
                 }
             });
 
-            var score = scores.Average() * (scores.Count / 40.0);
+            var score = scores.Average();
 
             Console.WriteLine("Edge Score: " + score * 100);
 
