1583fa3e5536e361c57c0d8797b894ee49ac16b0
dhbrett <DHBDev@outlook.com>
2016-04-13 11:39:11 -0700

    Generate UIAutomationCore.dll dynamically now

---
+++ generate_dll.ps1
@@ -0,0 +1,8 @@
+﻿if([System.IntPtr]::Size -eq 8){ #64 bit
+    & 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\TlbImp.exe' `
+    C:\Windows\System32\UIAutomationCore.dll /out:Interop.UIAutomationCore.dll
+}
+else { #32 bit
+    & 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\TlbImp.exe' `
+    C:\Windows\System32\UIAutomationCore.dll /out:Interop.UIAutomationCore.dll
+}
