b8d67923dcaa82c8093333996bb1f014a245bc25
dhbrett <DHBDev@outlook.com>
2016-04-13 14:45:59 -0700

    Revert "Generate UIAutomationCore.dll dynamically now" It turns out generating the dll automatically doesn't work because of necessary dependencies.
    This reverts commit 1583fa3e5536e361c57c0d8797b894ee49ac16b0.

--- generate_dll.ps1
+++
@@ -1,8 +0,0 @@
-﻿if([System.IntPtr]::Size -eq 8){ #64 bit
-    & 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\x64\TlbImp.exe' `
-    C:\Windows\System32\UIAutomationCore.dll /out:Interop.UIAutomationCore.dll
-}
-else { #32 bit
-    & 'C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\TlbImp.exe' `
-    C:\Windows\System32\UIAutomationCore.dll /out:Interop.UIAutomationCore.dll
-}
--- run.ps1
+++ run.ps1
@@ -2,10 +2,6 @@ if(-Not (Test-Path .\nuget.exe)){
     (New-Object net.WebClient).DownloadFile('https://nuget.org/nuget.exe', 'nuget.exe')
 }
 
-if(-Not (Test-Path .\Interop.UIAutomationCore.dll)){
-    Invoke-Expression -Command generate_dll.ps1
-}
-
 nuget.exe restore A11y.sln -Verbosity quiet
 
 $msbuild = Get-Item C:\Windows\Microsoft.NET\Framework\v4*\MSBuild\
