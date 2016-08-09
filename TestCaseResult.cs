0755bf5209180e8b61f16fe3ad8bfaf0b48877ec
dhbrett <DHBDev@outlook.com>
2016-08-09 15:21:39 -0700

    Style changes for TestCaseResult.cs

--- TestCaseResult.cs
+++ TestCaseResult.cs
@@ -2,45 +2,73 @@
 {
     using System.Text;
 
+    /// <summary>
+    /// The different types of results that can be returned from a test
+    /// </summary>
     public enum ResultType
     {
+        /// <summary>
+        /// A complete failure of the test
+        /// </summary>
         Fail = 0,
+
+        /// <summary>
+        /// A complete pass of the test
+        /// </summary>
         Pass = 1,
+
+        /// <summary>
+        /// An unknown result
+        /// </summary>
         Unknown = 2,
+
+        /// <summary>
+        /// A partial failure of the test
+        /// </summary>
         Half = 3
     }
 
+    /// <summary>
+    /// The container for results returned from tests
+    /// </summary>
     public class TestCaseResult
     {
+        /// <summary>
+        /// Gets or sets the result enum
+        /// </summary>
         public ResultType Result { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the element being tested
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets an explanation of the result
+        /// </summary>
         public string MoreInfo { get; set; }
+
+        /// <summary>
+        /// Gets or sets which browser executed the tests
+        /// </summary>
         public string Browser { get; set; }
 
+        /// <summary>
+        /// Gets or sets summarizes the result into a form that can be printed to the console
+        /// </summary>
+        /// <returns>A space-separated set of values describing the result</returns>
         public override string ToString()
         {
-            return Name + ":\n\t" + Result + (MoreInfo != null ? " (" + MoreInfo + ")" : "");
+            return this.Name + ":\n\t" + this.Result + (this.MoreInfo != null ? " (" + this.MoreInfo + ")" : string.Empty);
         }
 
+        /// <summary>
+        /// Summarizes the result into a form that can be added to a csv file
+        /// </summary>
+        /// <returns>A comma-separated set of values describing the result</returns>
         public string ToCSVString()
         {
-            return Name + "," + Result + (MoreInfo != null ? "," + MoreInfo : "");
-        }
-    }
-
-
-    public class TestCaseResultExt : TestCaseResult
-    {
-        public TestCaseResultExt()
-        {
-            Result = ResultType.Pass;
-        }
-
-        public void AddInfo(string info)
-        {
-            var stringBuilder = new StringBuilder(MoreInfo);
-            stringBuilder.Append(info);
-            MoreInfo = stringBuilder.ToString();
+            return this.Name + "," + this.Result + (this.MoreInfo != null ? "," + this.MoreInfo : string.Empty);
         }
     }
 }
---
+++ TestCaseResultExt.cs
@@ -0,0 +1,29 @@
+﻿namespace Microsoft.Edge.A11y
+{
+    using System.Text;
+
+    /// <summary>
+    /// A modified version of TestCaseResult that is used for sub-tests
+    /// </summary>
+    public class TestCaseResultExt : TestCaseResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestCaseResultExt" /> class
+        /// </summary>
+        public TestCaseResultExt()
+        {
+            this.Result = ResultType.Pass;
+        }
+
+        /// <summary>
+        /// Adds information about the ongoing set of tests
+        /// </summary>
+        /// <param name="info">A string describing the state of the tests</param>
+        public void AddInfo(string info)
+        {
+            var stringBuilder = new StringBuilder(MoreInfo);
+            stringBuilder.Append(info);
+            this.MoreInfo = stringBuilder.ToString();
+        }
+    }
+}
